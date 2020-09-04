642bdb643f15b9b76ef0e9d6bcc389a458858d95
Juan Mireles II <jmireles2dev@gmail.com>
2020-09-03 23:14:09 -0500

    template added with question info

---
+++ chapter02/programmingChallenges/02_25_initials.c
@@ -0,0 +1,16 @@
+/* Filename: 02_25_initials.c
+   Title: Initials
+
+   Name: Juan Mireles II
+   Course: COSC 1320.V01 - C Programming
+   Term: Fall 2020
+   Institution: South Texas College
+   Instructor: Mr. Joseph Perez
+   Date: 
+   Due: Sunday, September 13, 2020
+   Notes: Print your initials in block letters down the page. Construct each 
+          block letter out of the letter it represents as shown below.  
+          (e.g. with my initials JMP)
+*/
+
+
