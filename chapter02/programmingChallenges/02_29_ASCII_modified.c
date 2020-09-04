469f8ce2ee8ea2b033d7583d0cf12c85a0d86af0
Juan Mireles II <jmireles2dev@gmail.com>
2020-09-03 23:12:48 -0500

    template added with question info

---
+++ chapter02/programmingChallenges/02_29_ASCII_modified.c
@@ -0,0 +1,27 @@
+/* Filename: 02_29_ASCII_modified.c
+   Title:  ASCII Modified by Instructor
+   
+   Name: Juan Mireles II
+   Course: COSC 1320.V01 - C Programming
+   Term: Fall 2020
+   Institution: South Texas College
+   Instructor: Mr. Joseph Perez
+   Date: 
+   Due: Sunday, September 13, 2020 by 11:59PM CDT
+   Notes: Here's a peek ahead. In this chapter you learned about integers and 
+          the type int. C can also represent uppercase letters, lowercase 
+          letters and a considerable variety of special symbols. C uses small 
+          integers internally to represent each different character. C uses 
+          small integers internally (ASCII) to represent each different 
+          character. The set of characters a computer uses together with the 
+          corresponding integer representations for those characters is called 
+          that computer's character set. You can print the integer equivalent 
+          of uppercase A, for example, by executing the statement below.  
+          Another example is given on the next line.
+
+             printf("%d",'A');
+
+            printf("%d %d",'A','B');
+
+          Write a C program that prints your name in ASCII representation.
+*/
