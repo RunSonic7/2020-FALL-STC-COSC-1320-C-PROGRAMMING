ddf8a5432a13067d347f867ef541e9f21e673414
Juan Mireles II <jmireles2dev@gmail.com>
2020-09-27 05:32:54 -0500

    Initial Commit

---
+++ module03/challenges/03_17_creditLimitCalc.c
@@ -0,0 +1,25 @@
+/* Filename: 03_17_creditLimitCalc.c
+   Title:  Credit-Limit Calculator
+   
+   Name: Juan Mireles II
+   Course: COSC 1320.V01 - C Programming
+   Term: Fall 2020
+   Institution: South Texas College
+   Instructor: Mr. Joseph Perez
+   Due: Sunday, September 27, 2020
+   Notes: Develop a C program that will determine whether a department store 
+          customer has exceeded the credit limit on a charge account. For each 
+          customer, the following facts are available:
+
+            Account number (4 digits)
+            Balance at the beginning of the month
+            Total of all items charges by this customer this month
+            Total of all credits applied to this customer's account this month
+            Allowed credit limit
+
+          The program should input each fact, calculate the balance 
+          (=balance + charges - credits), and determine whether the new balance 
+          exceeds the customer's credit limit. For those customers whose credit 
+          limit is exceeded, the program should display the customer's account 
+          number, credit limit, new balance and the message "Credit limit exceeded." 
+*/
