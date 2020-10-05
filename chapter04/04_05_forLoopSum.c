df5aad2d278cbde62c60fec8268895f89c79ed41
Juan Mireles II <jmireles2dev@gmail.com>
2020-10-04 23:50:29 -0500

    Program Complete

---
+++ chapter04/04_05_forLoopSum.c
@@ -0,0 +1,29 @@
+/* Filename: 04_05_forLoopSum.c
+   Title:  Counter Controlled Repetition with the for statement to sum numbers.
+   
+   Name: Juan Mireles II
+   Course: COSC 1320.V01 - C Programming
+   Term: Fall 2020
+   Institution: South Texas College
+   Instructor: Mr. Joseph Perez
+   Due: Sunday, October 4, 2020
+   Notes: Fig. 4.5: fig04_05.c
+*/
+
+#include <stdio.h>
+
+/* Function main begins program execution */
+int main(void)
+{
+   int sum = 0; /* Initialize sum */
+   int number;  /* Number to be added to sum */
+
+   for (number = 2; number <= 100; number += 2)
+   {
+      sum += number; /* Add number to sum */
+   }
+
+   printf("Sum is %d\n", sum); /* Output sum */
+
+   return 0; /* Indiciate program ended successfully */
+} /* End function main */
