3c54d88ad7aa13d5fe0c1304683fd5a69ac3b49c
Juan Mireles II <jmireles2dev@gmail.com>
2020-12-02 21:05:31 -0600

    Working Code - Complete, need to refactor

--- module06/challenges/flightSystem2.c
+++ module06/challenges/flightSystem2.c
@@ -10,9 +10,9 @@ void printTicket(int, int);
 
 int main(void)
 {
-
     int seatingChart[SIZE] = { 0 };
     int userChoice;
+    int userSecondChoice;
     int classType;
     do
     {
@@ -28,15 +28,14 @@ int main(void)
         }
 
         classType = userChoice;
+        bool isAvailable;
+        int positionOpen;
         // Check system for seat options
         switch (userChoice)
         {
         case 1:
             printf("=== FIRST CLASS SYSTEM ===\n");
             // Check if available
-            bool isAvailable;
-            int positionOpen;
-
             for (int i = 0; i <= FC; i++)
             {
                 if (seatingChart[i] == 0)
@@ -50,18 +49,64 @@ int main(void)
 
             if (isAvailable == false)
             {
-                printf("No First Class Available!\n");
+                printf("No First Class Available!\n"
+                    "Would you like a ticket in Economy?\n\n");
+
+
+                printf("[1] Yes\n[2] No\n\nChoice: ");
+                scanf("%d", &userSecondChoice);
+
+                while (userSecondChoice != 1 && userSecondChoice != 2)
+                {
+                    printf("\nInvalid Input. Please re-enter value.\n");
+                    printf("No First Class Available!\n"
+                        "Would you like a ticket in Economy?\n\n");
+
+                    printf("[1] Yes\n[2] No\n\nChoice: ");
+                    scanf("%d", &userSecondChoice);
+                }
+
+                // Check Economy here
+                if (userSecondChoice == 2)
+                {
+                    printf("The next plane leaves in 3 hours.\n");
+                }
+                else
+                {
+                    // perform check here
+                    for (int i = FC + 1; i <= ECO; i++)
+                    {
+                        if (seatingChart[i] == 0)
+                        {
+                            isAvailable = true;
+                            positionOpen = i;
+                            break;
+                        }
+                        isAvailable = false;
+                    }
+
+                    // book ticket
+                    if (isAvailable == false)
+                    {
+                        printf("No more tickets. The next plane leaves in 3 hours.\n");
+                    }
+                    else
+                    {
+                        // Book Ticket
+                        seatingChart[positionOpen] = 1;
+                        printTicket(positionOpen + 1, classType);
+                    }
+                }
             }
             else
             {
                 // Mark Seat
                 seatingChart[positionOpen] = 1;
+                printTicket(positionOpen + 1, classType);
             }
-
-
-
             break;
         case 2:
+            printf("=== ECONOMY SYSTEM ===");
             for (int i = FC + 1; i <= ECO; i++)
             {
                 if (seatingChart[i] == 0)
@@ -75,12 +120,62 @@ int main(void)
 
             if (isAvailable == false)
             {
-                printf("No Economy Available!\n");
+                printf("No Economy Available!\n"
+                    "Would you like a ticket in First Class?\n\n");
+
+                printf("[1] Yes\n[2] No\n\nChoice: ");
+                scanf("%d", &userSecondChoice);
+
+                while (userSecondChoice != 1 && userSecondChoice != 2)
+                {
+                    printf("\nInvalid Input. Please re-enter value.\n");
+                    printf("No Economy Available!\n"
+                        "Would you like a ticket in First Class?\n\n");
+
+                    printf("[1] Yes\n[2] No\n\nChoice: ");
+                    scanf("%d", &userSecondChoice);
+                }
+
+                // Check FC Here
+                if (userChoice == 2)
+                {
+                    printf("The next plane leaves in 3 hours.\n");
+
+                }
+                else
+                {
+                    for (int i = 0; i <= FC; i++)
+                    {
+                        if (seatingChart[i] == 0)
+                        {
+                            isAvailable = true;
+                            positionOpen = i;
+                            break;
+                        }
+                        isAvailable = false;
+                    }
+
+                    if (isAvailable == false)
+                    {
+                        printf("No more tickets. The next plane leaves in 3 hours.\n");
+                    }
+                    else
+                    {
+                        // book ticket
+                        seatingChart[positionOpen] = 1;
+                        printTicket(positionOpen + 1, classType);
+                    }
+                }
+
+
+
+
             }
             else
             {
                 // Mark Seat
                 seatingChart[positionOpen] = 1;
+                printTicket(positionOpen + 1, classType);
             }
             break;
         default:
@@ -121,10 +216,8 @@ void printTicket(int seatNo, int class)
     printf("+----------------------------------------------+\n"
         "| B D   P |  AIRLINE: C AIRLINES               |\n"
         "| O I   A |                                    |\n"
-        "| A N   S |  CLASS: %d                        |\n"
-        "| R G   S |  SEAT NO: %d                      |\n"
+        "| A N   S |  CLASS: %d                          |\n"
+        "| R G   S |  SEAT NO: %d                        |\n"
         "+----------------------------------------------+\n", class, seatNo);
 }
 
-/* Source
-Strings https://www.geeksforgeeks.org/strings-in-c-2/ */
